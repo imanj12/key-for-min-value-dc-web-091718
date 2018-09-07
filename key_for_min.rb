6c5e0ac7b427fddca5bd85f65a4fda9662650cba
Iman Jawad <imanj12@gmail.com>
2018-09-07 18:07:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,18 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-    descending_array = []
-    last = 0
+    keys_array = []
+    last_value = 0
     name_hash.each do |key, value|
+      if last_value == 0
+        last_value = value
+        last_key = key
+      elsif value > last_value
+        keys_array << last_key
+      else
+        
+      end  
+      
       
       
       
