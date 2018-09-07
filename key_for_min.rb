96cd3f9b0f31a648535be371220891bd27d15f4d
Iman Jawad <imanj12@gmail.com>
2018-09-07 17:57:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,6 +10,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if (last*value)/2 > value
         descending_array << value
+      end
       last = value  
     end  
   end
